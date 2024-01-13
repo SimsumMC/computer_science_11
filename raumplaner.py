b5d9dfd0e5740569fbce86cafe3cb8226abda748
Niklas <niklas.hh@icloud.com>
2024-01-13 15:05:31 +0100

    Update raumplaner.py

--- raumplaner.py
+++ raumplaner.py
@@ -25,43 +25,8 @@ class MyApp(wx.App):
 
     def TestAnwendung(self):
         """Allein fuer die Testanwendung:"""
-        """
-        tisch_1 = Tisch(True)
-        tisch_1.BewegeHorizontal(50)
-        tisch_1.BewegeVertikal(50)
-
-        stuhl_1 = Stuhl(True)
-        stuhl_1.BewegeHorizontal(50)
-        stuhl_1.BewegeVertikal(40)
-
-        stuhl_4 = Stuhl(True)
-        stuhl_4.BewegeHorizontal(230)
-        stuhl_4.BewegeVertikal(40)
-        stuhl_4.Drehe(180)
-
-        stuhl_2 = Stuhl(True)
-        stuhl_2.BewegeHorizontal(110)
-        stuhl_2.BewegeVertikal(-20)
-        stuhl_2.Drehe(90)
-
-        stuhl_3 = Stuhl(True)
-        stuhl_3.BewegeHorizontal(170)
-        stuhl_3.BewegeVertikal(-20)
-        stuhl_3.Drehe(90)
-
-        stuhl_5 = Stuhl(True)
-        stuhl_5.BewegeHorizontal(110)
-        stuhl_5.BewegeVertikal(100)
-        stuhl_5.Drehe(-90)
-
-        stuhl_6 = Stuhl(True)
-        stuhl_6.BewegeHorizontal(170)
-        stuhl_6.BewegeVertikal(100)
-        stuhl_6.Drehe(-90)
-        """
-
-        x = MoreShelfs()
-        x.Drehe(90)
+        stuhl_1 = Stuhl(x, y, b, t, w, f, True)
+        
 
 
 
